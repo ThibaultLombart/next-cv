d56a09453a9c08e7149ba0ec3e89912e70482c06
Lombart Thibault <lombart.thibault@gmail.com>
2024-10-18 09:16:53 +0200

    essai

--- app/components/secondPageComponent.tsx
+++ app/components/secondPageComponent.tsx
@@ -1,43 +1,42 @@
 import React from 'react';
 import Image from "next/image";
 import { Secular_One } from "next/font/google";
-import styles from "../styles/secondPage.module.css";
 
 const secular_one = Secular_One({subsets: ["latin"],display: "swap", weight: "400"});
 
 const SecondPageComponent: React.FC = () => {
     return (
-        <section className={styles.secondpage}>
+        <section className={`secondpage`}>
         <h2 className={secular_one.className}>Skills</h2>
         <div>
           <h3>Web</h3>
-          <div className={styles.cardlist}>
+          <div className={`cardlist`}>
 
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>HTML</h4>
               <Image src="./images/html.svg" alt="HTML" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>CSS</h4>
               <Image src="./images/css.svg" alt="CSS" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>JS</h4>
               <Image src="./images/javascript.svg" alt="javascript" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>TS</h4>
               <Image src="./images/typescript.svg" alt="typescript" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>VUE</h4>
               <Image src="./images/vue.svg" alt="vue" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>NUXT</h4>
               <Image src="./images/nuxt.svg" alt="nuxt" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>LARAVEL</h4>
               <Image src="./images/laravel.svg" alt="laravel" width={100} height={100} />
             </div>
@@ -47,21 +46,21 @@ const SecondPageComponent: React.FC = () => {
 
         <div>
           <h3>Programming</h3>
-          <div className={styles.cardlist}>
+          <div className={`cardlist`}>
 
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Java</h4>
               <Image src="./images/java.svg" alt="JAVA" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Python</h4>
               <Image src="./images/python.svg" alt="PYTHON" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Lua</h4>
               <Image src="./images/lua.svg" alt="LUA" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Bash</h4>
               <Image src="./images/bash.svg" alt="BASH" width={100} height={100} />
             </div>
@@ -71,17 +70,17 @@ const SecondPageComponent: React.FC = () => {
 
         <div>
           <h3>Databases</h3>
-          <div className={styles.cardlist}>
+          <div className={`cardlist`}>
 
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>SQL</h4>
               <Image src="./images/sql.svg" alt="SQL" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>MongoDB</h4>
               <Image src="./images/mongodb.svg" alt="MONGODB" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Redis</h4>
               <Image src="./images/redis.svg" alt="REDIS" width={100} height={100} />
             </div>
@@ -91,21 +90,21 @@ const SecondPageComponent: React.FC = () => {
 
         <div>
           <h3>Testing</h3>
-          <div className={styles.cardlist}>
+          <div className={`cardlist`}>
 
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Selenium</h4>
               <Image src="./images/selenium.svg" alt="SELENIUM" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Cypress</h4>
               <Image src="./images/cypress.svg" alt="CYPRESS" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Robot Framework</h4>
               <Image src="./images/robot.svg" alt="ROBOT FRAMEWORK" width={100} height={100} />
             </div>
-            <div className={styles.card}>
+            <div className={`card`}>
               <h4>Appium</h4>
               <Image src="./images/appium.svg" alt="APPIUM" width={100} height={100} />
             </div>
--- app/components/thirdPageComponent.tsx
+++ app/components/thirdPageComponent.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
 
-import styles from '../styles/thirdPage.module.css';
-
 const ThirdPageComponent: React.FC = () => {
     return (
         <section>
         <h3>Projects</h3>
-        <div className={styles.list}>
+        <div className={`list_third`}>
 
-          <div className={styles.card}>
+          <div className={`card_third`}>
             <h4>TeaEnchants</h4>
             <p>Minecraft plugin for adding new enchantments</p>
             <Image src="./images/minecraft.jpg" alt="Plugin TeaEnchants" width={600} height={200} />
             <a href="https://github.com/ThibaultLombart/teaenchants" target="_blank"><button className="button">Go see on Github</button></a>
           </div>
-          <div className={styles.card}>
+          <div className={`card_third`}>
             <h4>Portfolio</h4>
             <p>Website Portfolio made in Laravel with HTML/CSS</p>
             <Image src="./images/site.jpg" alt="SITE" width={600} height={200} />
